f7501bfcea92e6be41dea0779e802d019490e281
Lmoran <luismoran0293@gmail.com>
2020-06-16 21:09:23 -0500

    end Error 404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,6 +1,9 @@
 const Error404 = () => {
   const view = `
     <div class = "Error404">
+    <div class = "Error-image">
+    <img src = "https://i.imgur.com/DkUmbo2.png">
+    </div>
       <h2>Error 404</h2>
     </div>
   `
